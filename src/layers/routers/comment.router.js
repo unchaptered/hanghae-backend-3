5628cf20520d1258f97925e576d5008f48d34f8c
wnlwjsan@gmail.com <wnlwjsan@gmail.com>
2022-08-08 18:33:13 +0900

    modified `comment.controller.js, comment.router.js`

--- src/layers/routers/comment.router.js
+++ src/layers/routers/comment.router.js
@@ -3,6 +3,13 @@ const commentController = require('../controllers/comment.controller');
 
 const commentRouter = express.Router();
 
-userRouter.post('', commentController.Comment);
+commentRouter.route('')
+    .get(commentController.getComment)
+    .post(commentController.createComment);
+
+commentRouter.route('/:articleId')
+    .get(commentController.getCommentById)
+    .put(commentController.updateCommentById)
+    .delete(commentController.deleteCommentById);
 
 module.exports = commentRouter;
