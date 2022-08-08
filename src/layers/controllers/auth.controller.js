9bc71228af5c0687ed3cabab4f174bc79607a3eb
jc <codeing999@gmail.com>
2022-08-08 15:50:15 +0900

    user에서 auth로 이름들 변경

---
+++ src/layers/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const authService = require('../services/auth.service');
+
+const join = (req, res, next) => {
+};
+
+const login = (req, res, next) => {
+};
+
+
+module.exports = {
+    join,
+    login
+}
