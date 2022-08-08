91f21ae33cb1671f10a1541e47914d3a5621ec19
Unchaptered <workstation19961002@gmail.com>
2022-08-08 16:17:25 +0900

    Update `**/article.controller.js`

--- src/layers/controllers/article.controller.js
+++ src/layers/controllers/article.controller.js
@@ -6,7 +6,7 @@ const getArticle = async (req, res, next) => {
 
 const createArtilce = async (req, res, next) => {
 
-    const test_user_id = 1;
+    const testUserId = 1;
     const { title, content } = req.body;
 
     try {
@@ -26,12 +26,70 @@ const createArtilce = async (req, res, next) => {
 
 }
 
-const updateArticleById = async (req, res, next) => {
-}
-const deleteArticleById = async (req, res, next) => {
-}
 
 const getArticleById = async (req, res, next) => {
+
+    const { articleId } = req.params;
+
+    try {
+
+        const result = await joi.object({
+            articleId: joi.number().required()
+        }).validateAsync({ articleId });
+
+        return res.json(result);
+
+    } catch(err) {
+
+        return res.json(err.message);
+
+    }
+
+}
+
+const updateArticleById = async (req, res, next) => {
+
+    const testUserId = 10;
+    const { articleId } = req.params;
+    const { title, content } = req.body;
+
+    try {
+
+        const result = await joi.object({
+            userId: joi.number().required(),
+            articleId: joi.number().required(),
+            title: joi.string().min(1).max(50).required(),
+            content: joi.string().min(1).max(250).required()
+        }).validateAsync({ userId: testUserId, articleId, title, content });
+
+        return res.json(result);
+
+    } catch(err) {
+
+        return res.json(err.message);
+
+    }
+    
+}
+const deleteArticleById = async (req, res, next) => {
+
+    const testUserId = 10;
+    const { articleId } = req.params;
+
+    try {
+
+        const result = await joi.object({
+            userId: joi.number().required(),
+            articleId: joi.number().required()
+        }).validateAsync({ userId: testUserId, articleId });
+
+        return res.json(result);
+
+    } catch(err) {
+
+        return res.json(err.message);
+
+    }
 }
 
 module.exports = {
