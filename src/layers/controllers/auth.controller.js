6df348ef3e0e5017c03c1e872fa77ce39aa5df5c
jc <codeing999@gmail.com>
2022-08-09 04:04:32 +0900

    임시

--- src/layers/controllers/auth.controller.js
+++ src/layers/controllers/auth.controller.js
@@ -3,32 +3,42 @@ const Joi = require("joi");
 
 const join = async (req, res, next) => {
 
-    const authDto = await Joi.object({
-        nickname : Joi.toString().min(2).max(15).required(),
-        password : Joi.toString().min(4).max(20).required(),
-        confirm : Joi.toString().min(4).max(20).required(),
-    }).validateAsync({ ...req.body} );
-    
-    if (password !== confirm) {
-        return res.status(400).send({
-            erroMessage : "패스워드와 패스워드 확인이 일치하지 않습니다."
+    try {
+        const joinDto = await Joi.object({
+            nickname: Joi.string().min(2).max(15).required(),
+            password: Joi.string().min(4).max(20).required(),
+            confirm: Joi.string().min(4).max(20).required()
+        }).validateAsync( {...req.body} );
 
-        });
-    };
+        res.send("d")
 
-    return ;
+        if (password !== confirm) {
+            return res.status(400).send({
+                erroMessage: "패스워드와 패스워드 확인이 일치하지 않습니다."
 
-    //const IS_EXIST = await 
+            });
+        };
+        //const IS_EXIST = await
+        const CREATE_USER = `
+            INSERT INTO user 
+        `
+        //const user = 
+        
+    } catch (err) {
+        return res.json(err.message);
+    }
+
+
 };
 
 const login = async (req, res, next) => {
 
     const authDto = await Joi.object({
-        nickname : Joi.toString().min(2).max(15).required(),
-        password : Joi.toString().min(4).max(20).required(),
-    }).validateAsync({ ...req.body} );
+        nickname: Joi.toString().min(2).max(15).required(),
+        password: Joi.toString().min(4).max(20).required(),
+    }).validateAsync({ ...req.body });
 
-    return ;
+    return;
 };
 
 
