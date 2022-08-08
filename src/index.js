06d6d309b33e65414a542dfda3ce9d71a919bcc0
Unchaptered <workstation19961002@gmail.com>
2022-08-08 15:29:59 +0900

    Apply `**/article.router.js` into `src/index.js`

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const authRouter = require('./layers/routers/user.router');
+const articleRouter = require('./layers/routers/article.router');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRouter);
+app.use('/article', articleRouter);
 
 app.listen(3000, () => console.log(`Server is running on 3000`));
