8625e8a1187690f13637e3e1a9d3e365f614a1d3
jc <codeing999@gmail.com>
2022-08-08 17:25:49 +0900

    api/readme.md에 body 작성

--- src/layers/controllers/auth.controller.js
+++ src/layers/controllers/auth.controller.js
@@ -1,25 +1,34 @@
 const authService = require('../services/auth.service');
 const Joi = require("joi");
 
-const join = (req, res, next) => {
+const join = async (req, res, next) => {
 
     const authDto = await Joi.object({
         nickname : Joi.toString().min(2).max(15).required(),
         password : Joi.toString().min(4).max(20).required(),
         confirm : Joi.toString().min(4).max(20).required(),
-    }).validateAsync({ ... req.body} );
+    }).validateAsync({ ...req.body} );
     
     if (password !== confirm) {
         return res.status(400).send({
             erroMessage : "패스워드와 패스워드 확인이 일치하지 않습니다."
 
         });
-    }
+    };
 
-    const IS_EXIST = await 
+    return ;
+
+    //const IS_EXIST = await 
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
+
+    const authDto = await Joi.object({
+        nickname : Joi.toString().min(2).max(15).required(),
+        password : Joi.toString().min(4).max(20).required(),
+    }).validateAsync({ ...req.body} );
+
+    return ;
 };
 
 
