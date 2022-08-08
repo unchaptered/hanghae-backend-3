98b79dc33a7f47132c9d29ef0acea76408da648f
jc <codeing999@gmail.com>
2022-08-08 16:09:30 +0900

    docs도 user->auth로 수정

--- src/layers/controllers/auth.controller.js
+++ src/layers/controllers/auth.controller.js
@@ -1,6 +1,12 @@
 const authService = require('../services/auth.service');
+const Joi = require("joi");
 
 const join = (req, res, next) => {
+
+    const authDto = await Joi.object({
+        
+    }).validateAsync({ ... req.body} );
+    
 };
 
 const login = (req, res, next) => {
