9eb777143e7b85583b47240d5e80e1d8edc1bb31
Rumaro122 <yeshin0122@gmail.com>
2022-08-09 23:10:30 +0900

    게시글 좋아요 기능 구현

--- src/layers/repositories/article.repository.js
+++ src/layers/repositories/article.repository.js
@@ -6,7 +6,10 @@ const mysql = require('mysql2');
  * @param { mysql.PoolConnection } poolConnection 
  * @param { number } userId 
  * @param { string } title 
- * @param { string } content : ;
+ * @param { string } content 
+ * @param { number } articleId
+ * @param { number } likeId
+ * @param { boolean } isLike: ;
  * @returns 
  */
 const createArticle = async (poolConnection, userId, title, content) => {
@@ -23,6 +26,61 @@ const createArticle = async (poolConnection, userId, title, content) => {
 
 }
 
+const isExists = async (poolConnection, articleId) => {
+
+    const isExistsQuery = `SELECT * FROM article WHERE article_id = ${articleId};`;
+    const queryResult = await poolConnection.query(isExistsQuery);
+    
+    const selectResult = queryResult[0];
+
+    return selectResult.length !== 0 ? true : false;
+
+}
+
+const isLikeExists = async (poolConnection, userId, articleId) => {
+
+    const isExistsQuery = `SELECT * FROM article_like WHERE article_id = ${articleId} AND user_Id = ${userId};`;
+    const queryResult = await poolConnection.query(isExistsQuery);
+    
+    const selectResult = queryResult[0];
+
+    return selectResult.length !== 0 ? true : false;
+
+}
+
+const createArticleLike = async (poolConnection, userId, articleId, isLike) => {
+    
+    const createQuery = `INSERT INTO article_like (user_id, article_id, is_liked) VALUES (${userId}, ${articleId}, ${isLike});`;
+    const queryResult = await poolConnection.query(createQuery);
+    
+    const insertResult = queryResult[0];
+
+    if (insertResult.affectedRows !== 1) return null;
+    else return ({
+        likeId: insertResult.insertId, userId, articleId, isLike
+    });
+
+}
+
+const updateArticleLike = async (poolConnection, userId, articleId, isLike) => {
+    
+    const updateQuery = `UPDATE article_like SET is_liked = ${isLike} WHERE user_Id = ${userId} AND article_Id = ${articleId};`;
+    const queryResult = await poolConnection.query(updateQuery);
+    
+    const insertResult = queryResult[0];
+
+    if (insertResult.affectedRows !== 1) return null;
+    else return ({
+        likeId: insertResult.insertId, userId, articleId, isLike
+    });
+
+}
+
+
 module.exports = {
-    createArticle
+    createArticle,
+    createArticleLike,
+    updateArticleLike,
+    isExists,
+    isLikeExists
 }
--- src/layers/services/article.service.js
+++ src/layers/services/article.service.js
@@ -33,6 +33,43 @@ const createArtilce = async (userId, title, content) => {
     
 }
 
+const updateArticleLike = async (userId, articleId, isLike) => {
+
+    const poolConnection = await pool.getConnection();
+
+    try {
+        
+        await poolConnection.beginTransaction();
+
+        // userId 가 테이블에 실존하는지 체크
+        const isUserExists = await authRepository.isExists(poolConnection, userId);
+        if (!isUserExists) throw new Error('존재하지 않는 사용자입니다.');       
+
+        const isArticleExists = await articleRepository.isExists(poolConnection, articleId);
+        if (!isArticleExists) throw new Error('존재하지 않는 게시물입니다.'); 
+
+        const isArticleLikeExists = await articleRepository.isLikeExists(poolConnection, userId, articleId);
+        if (isArticleLikeExists) await articleRepository.updateArticleLike(poolConnection, userId, articleId, isLike);        
+        else {await articleRepository.createArticleLike(poolConnection, userId, articleId, isLike );};
+        
+        await poolConnection.commit();
+        poolConnection.release();
+        
+        return "좋아요/ 좋아요 취소 적용";
+
+    } catch(err) {
+
+        await poolConnection.rollback();
+        poolConnection.release();
+
+        console.log(err)
+        return `${err.name} : ${err.message}`;
+
+    }
+    
+}
+
 module.exports = {
-    createArtilce
+    createArtilce,
+    updateArticleLike
 }
