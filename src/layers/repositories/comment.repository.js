723f0ea5e447f0dc26c1480237c601c80a3f2880
wnlwjsan@gmail.com <wnlwjsan@gmail.com>
2022-08-09 16:30:33 +0900

    update `comment.repository.js`

--- src/layers/repositories/comment.repository.js
+++ src/layers/repositories/comment.repository.js
@@ -1,7 +1,28 @@
-const Comment = () => {}
+const mysql = require('mysql2');
 
+/**
+ * 생성하는 거(행동, 책임) -> 결과 ? -> { 생성된 객체 } or null( 실패 )
+ * 
+ * @param { mysql.PoolConnection } poolConnection 
+ * @param { number } userId 
+ * @param { number } articleId
+ * @param { string } content : ;
+ * @returns 
+ */
+const createComment = async (poolConnection, userId, articleId, content) => {
+    
+    const createQuery = `INSERT INTO comment (user_id, articleId, content) VALUES (${userId}, '${articleId}', '${content}');`;
+    const queryResult = await poolConnection.query(createQuery);
 
+    const insertResult = queryResult[0];
+
+    if (insertResult.affectedRows !== 1) return null;
+    else return ({
+        commentId: insertResult.insertId, userId, articleId, content
+    });
+
+}
 
 module.exports = {
-    Comment
+    createComment
 }
