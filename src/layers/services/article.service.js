88fac40c85dbd51d0766022baa2c91e615e16d15
Unchaptered <workstation19961002@gmail.com>
2022-08-09 21:09:00 +0900

    Update `**/article.service.js`, 테스트 용 연결함수 생성

--- src/layers/services/article.service.js
+++ src/layers/services/article.service.js
@@ -2,7 +2,12 @@ const pool = require('../../db');
 const articleRepository = require('../repositories/article.repository');
 const authRepository = require('../repositories/auth.repository');
 
-/** @param { number } userId @param { string } title @param { string } content @returns { Promise< string > } */
+/**
+ * @param { number } userId
+ * @param { string } title
+ * @param { string } content
+ * @returns { Promise< { articleId: number, userId: number, title: string, content: string } >}
+ */
 const createArticle = async (userId, title, content) => {
 
     const poolConnection = await pool.getConnection();
@@ -15,13 +20,13 @@ const createArticle = async (userId, title, content) => {
         const isExists = await authRepository.isExists(poolConnection, userId);
         if (!isExists) throw new Error('존재하지 않는 사용자입니다.');
 
-        const isCreated = await articleRepository.createArticle(poolConnection, userId, title, content);
+        const cretedArticle = await articleRepository.createArticle(poolConnection, userId, title, content);
         if (isCreated === null) throw new Error('생성에 실패한 게시글입니다.');
 
         await poolConnection.commit();
         poolConnection.release();
 
-        return '게시글 작성에 성공하셨습니다.';
+        return cretedArticle;
 
     } catch(err) {
 
@@ -34,6 +39,37 @@ const createArticle = async (userId, title, content) => {
     
 }
 
+/**
+ * @param { number } articleId 
+ */
+const getArticleById = (articleId) => {
+
+}
+
+/**
+ * @param { number } userId 
+ * @param { number } articleId 
+ * @param { string } title 
+ * @param { string } content 
+ */
+const updateArticleById = (userId, articleId, title, content) => {
+
+}
+
+/**
+ * @param { number } userId 
+ * @param { number } articleId 
+ */
+const deleteArticleById = (userId, articleId) => {
+
+}
+
+
 module.exports = {
-    createArticle
+
+    createArticle,
+    getArticleById,
+    updateArticleById,
+    deleteArticleById,
+
 }
