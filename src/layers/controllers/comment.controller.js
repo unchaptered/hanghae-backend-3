95f0891591d764f21c2d38e5551bf20891d62d91
wnlwjsan@gmail.com <wnlwjsan@gmail.com>
2022-08-10 20:18:17 +0900

    update `comment.controller.js`

--- src/layers/controllers/comment.controller.js
+++ src/layers/controllers/comment.controller.js
@@ -1,26 +1,14 @@
+const e = require('express');
 const joi = require('joi');
 const commentService = require('../services/comment.service');
 
+/** @param { e.Request } req @param { e.Response } res @param { e.NextFunction } next */
 const getComment = async (req, res, next) => {
-}
-
-const createComment = async (req, res, next) => {
-
-    const testUserId = 1;
-    const testArticleId = 1;
-    
-    const { content } = req.body;
 
     try {
+            
+        const result = await commentService.getComment();
 
-        await joi.object({
-            userId: joi.number().required(),
-            articleId: joi.number().required(),
-            content: joi.string().min(1).max(250).required()
-        }).validateAsync({ userId: testUserId, articleId: testArticleId, content });
-
-        const result = await commentService.createComment(testUserId, testAritcleId,  title, content);
-        
         return res.status(200).json(result);
 
     } catch(err) {
@@ -31,6 +19,113 @@ const createComment = async (req, res, next) => {
 
     }
 
+}
+
+/** @param { e.Request } req @param { e.Response } res @param { e.NextFunction } next */
+const createComment = async (req, res, next) => {
+
+    const testUserId = 1;
+    const testArticleId = 1;
+    const { content } = req.body;
+
+    try {
+
+        await joi.object({
+            userId: joi.number().required(),
+            articleId: joi.number().required(),
+            content: joi.string().min(1).max(250).required()
+        }).validateAsync({ userId: testUserId, articleId: testArticleId, content });
+
+        const result = await commentService.createComment(testUserId, testArticleId, content);
+
+        return res.status(200).json(result);
+
+    } catch(err) {
+
+        console.log(err);
+
+        return res.status(500).json(err.message);
+
+    }
+
+}
+
+/** @param { e.Request } req @param { e.Response } res @param { e.NextFunction } next */
+const getCommentById = async (req, res, next) => {
+
+    const { commentId } = req.params;
+
+    try {
+
+        await joi.object({
+            commentId: joi.number().required()
+        }).validateAsync({ commentId });
+
+        const result = await commentService.getCommentById(commentId);
+
+        return res.json(result);
+
+    } catch(err) {
+
+        return res.json(err.message);
+
+    }
+
+}
+
+/** @param { e.Request } req @param { e.Response } res @param { e.NextFunction } next */
+const updateCommentById = async (req, res, next) => {
+
+    const testUserId = 1;
+    const testArticleId = 1;
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    try {
+
+        await joi.object({
+            userId: joi.number().required(),
+            articleId: joi.number().required(),
+            commentId: joi.number().required(),
+            content: joi.string().min(1).max(250).required()
+        }).validateAsync({ userId: testUserId, articleId: testArticleId, content });
+
+        const result = await commentService.updateCommentById(testUserId, testArticleId, commentId, content);
+
+        return res.json(result);
+
+    } catch(err) {
+
+        return res.json(err.message);
+
+    }
+    
+}
+
+/** @param { e.Request } req @param { e.Response } res @param { e.NextFunction } next */
+const deleteCommentById = async (req, res, next) => {
+
+    const testUserId = 1;
+    const testArticleId = 1;
+    const { commentId } = req.params;
+
+    try {
+
+        await joi.object({
+            userId: joi.number().required(),
+            articleId: joi.number().required(),
+            commentId: joi.number().required()
+        }).validateAsync({ userId: testUserId, articleId: testArticleId });
+
+        const result = await commentService.deleteCommentById(testUserId, testArticleId, commentId);
+        
+        return res.json(result);
+
+    } catch(err) {
+
+        return res.json(err.message);
+
+    }
 }
 
 const updateCommentLike = async (req, res, next) => {
@@ -58,73 +153,8 @@ const updateCommentLike = async (req, res, next) => {
     }
 };
 
-const getCommentById = async (req, res, next) => {
 
-    const { commentId } = req.params;
 
-    try {
-
-        const result = await joi.object({
-            commentId: joi.number().required()
-        }).validateAsync({ commentId });
-
-        return res.json(result);
-
-    } catch(err) {
-
-        return res.json(err.message);
-
-    }
-
-}
-
-const updateCommentById = async (req, res, next) => {
-
-    const testUserId = 10;
-    const testArticeId = 10;
-    const { commentId } = req.params;
-    const { content } = req.body;
-
-    try {
-
-        const result = await joi.object({
-            userId: joi.number().required(),
-            articleId: joi.number().required(),
-            commentId: joi.number().required(),
-            content: joi.string().min(1).max(250).required()
-        }).validateAsync({ userId: testUserId, articleId: testArticeId, commentId, content });
-
-        return res.json(result);
-
-    } catch(err) {
-
-        return res.json(err.message);
-
-    }
-    
-}
-const deleteCommentById = async (req, res, next) => {
-
-    const testUserId = 10;
-    const testArticleId = 10;
-    const { commentId } = req.params;
-
-    try {
-
-        const result = await joi.object({
-            userId: joi.number().required(),
-            articleId: joi.number().required(),
-            commentId: joi.number().required()
-        }).validateAsync({ userId: testUserId, articleId:testArticleId, commentId });
-
-        return res.json(result);
-
-    } catch(err) {
-
-        return res.json(err.message);
-
-    }
-}
 
 module.exports = {
 
