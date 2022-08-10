1624275a10c1008bbab5430e203a156e0e4c33de
wnlwjsan@gmail.com <wnlwjsan@gmail.com>
2022-08-10 20:16:15 +0900

    update `comment.repository`

--- src/layers/repositories/comment.repository.js
+++ src/layers/repositories/comment.repository.js
@@ -1,18 +1,14 @@
 const mysql = require('mysql2');
 
 /**
- * 생성하는 거(행동, 책임) -> 결과 ? -> { 생성된 객체 } or null( 실패 )
  * 
  * @param { mysql.PoolConnection } poolConnection 
- * @param { number } userId 
- * @param { number } articleId
- * @param { string } content : ;
- * @returns 
+ * @param { number } commentId 
+ * @returns { Promise< boolean > }
  */
+ const isExistsArticle = async (poolConnection, commentId) => {
 
- const isExistsArticle = async (poolConnection, userId) => {
-
-    const isExistsQuery = `SELECT * FROM article WHERE article_id = ${articleId};`;
+    const isExistsQuery = `SELECT * FROM comment WHERE comment_id = ${commentId};`;
     const queryResult = await poolConnection.query(isExistsQuery);
 
     const selectResult = queryResult[0];
@@ -20,17 +16,94 @@ const mysql = require('mysql2');
     return selectResult.length !== 0 ? true : false;
 
 }
+/**
+ * 
+ * @param { mysql.poolConnection } poolConnection 
+ * @returns { Promise< Array<{ articleId: number, userId: number, commentId: number, content: string }> > }
+ */
+ const getComment = async (poolConnection) => {
+
+    const selectQuery = `SELECT comment_id as commentId, user_id as userId, article_id as articleId, content FROM comment LIMIT 100`;
+    const queryResult = await poolConnection.query(selectQuery);
+
+    const selectResult = queryResult[0];
+
+    return selectResult;
+    
+}
+/**
+ * 
+ * @param { mysql.poolConnection } poolConnection 
+ * @param { number } commentId 
+ * @returns { Promise< { articleId: number, userId: number, commentId: number, content: string } | null >}
+ */
+const getCommentById = async (poolConnection, commentId) => {
+
+    const selectQuery = `SELECT comment_id as commentId, user_id as userId, article_id as articleId, content FROM comment WHERE comment_id = ${commentId};`;
+    const queryResult = await poolConnection.query(selectQuery);
+
+    const selectResult = queryResult[0];
+    
+    return selectResult.length !== 0 ? selectResult[0] : null;
+
+}
+
+/**
+ * 
+ * @param { mysql.PoolConnection } poolConnection 
+ * @param { number } userId 
+ * @param { number } articleId 
+ * @param { string } content
+ * @returns { Promise< { commentId: number, userId: number, articleId:number, content: string } | null > }
+ */
 const createComment = async (poolConnection, userId, articleId, content) => {
     
-    const createQuery = `INSERT INTO comment (user_id, articleId, content) VALUES (${userId}, '${articleId}', '${content}');`;
+    const createQuery = `INSERT INTO comment (user_id, article_Id, content) VALUES (${userId}, '${articleId}', '${content}');`;
     const queryResult = await poolConnection.query(createQuery);
 
     const insertResult = queryResult[0];
 
     if (insertResult.affectedRows !== 1) return null;
     else return ({
-        commentId: insertResult.insertId, userId, articleId, content
+        articleId: insertResult.insertId, userId, articleId, content
     });
+
+}
+
+/**
+ * 
+ * @param { mysql.PoolConnection } poolConnection 
+ * @param { number } articleId 
+ * @param { number } commentId 
+ * @param { string } content 
+ * @returns { Promise< boolean > }
+ */
+const updateCommentById = async (poolConnection, commentId, content) => {
+
+    const updateQuery = `UPDATE comment SET content = '${content}' WHERE comment_id = ${commentId};`;
+    const queryResult = await poolConnection.query(updateQuery);
+
+    const updateResult = queryResult[0];
+
+    return updateResult.affectedRows === 1 ? true : false;
+
+}
+
+
+/**
+ * 
+ * @param { mysql.PoolConnection } poolConnection 
+ * @param { number } commentId 
+ * @returns { Promise< boolean> }
+ */
+ const deleteCommentById = async (poolConnection, commentId) => {
+    
+    const deleteQuery = `DELETE FROM comment WHERE comment_id = ${commentId};`;
+    const queryResult = await poolConnection.query(deleteQuery);
+
+    const deleteResult = queryResult[0];
+
+    return deleteResult.affectedRows === 1 ? true : false;
 
 }
 
@@ -91,6 +164,13 @@ module.exports = {
     updateCommentLike,
     isExists,
     isLikeExists,
+    isExistsArticle,
+    getComment,
+    getCommentById,
     createComment,
-    isExistsArticle
+    updateCommentById,
+    createCommentLike,
+    deleteCommentById,
+    updateCommentLike,
+    
 }
