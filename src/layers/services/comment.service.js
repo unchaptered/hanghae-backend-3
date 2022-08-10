30bc352faae02ae8c1a4d6248034451a6c890500
wnlwjsan@gmail.com <wnlwjsan@gmail.com>
2022-08-10 14:20:00 +0900

    update `comment.service.js`

--- src/layers/services/comment.service.js
+++ src/layers/services/comment.service.js
@@ -1,9 +1,35 @@
+const pool = require('../../db');
+const articleRepository = require('../repositories/article.repository');
 const commentRepository = require('../repositories/comment.repository');
 
-const Comment = () => {
+const createComment = async (userId, articleId, content) => {
+    const poolConnection = await pool.getConnection();
+
+    try {
+
+        await poolConnection.beginTransaction();
+
+        // articleId 가 테이블에 실존하는지 체크
+        const isExistsArticle = await articleRepository.isExistsArticle(poolConnection, articleId);
+        if (!isExistsArticle) throw new Error('존재하지 않는 게시글입니다.');
+
+        const isCreated = await commentRepository.createArtilce(poolConnection, userId, articleId, content);
+        if (isCreated === null) throw new Error('생성에 실패한 댓글입니다.');
+
+        await poolConnection.commit();
+        poolConnection.release();
+
+        return '댓글 작성에 성공하셨습니다.';
+
+    } catch(err) {
+
+        await poolConnection.rollback();
+        poolConnection.release();
+        return `${err.name} : ${err.message}`;
+    }
+    
 }
 
-
 module.exports = {
-    Comment
+    createComment
 }
