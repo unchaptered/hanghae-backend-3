bd72a943d9f07104914c47e8401cd1456cc661aa
Unchaptered <workstation19961002@gmail.com>
2022-08-11 17:26:31 +0900

    Update `src/index.js`

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const commentRouter = require('./layers/routers/comment.router');
 
 const app = express();
 
+console.log('hello');
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
