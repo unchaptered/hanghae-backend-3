56aa95b2f8ba4c48afeba4c7b7566be4212c32df
Unchaptered <workstation19961002@gmail.com>
2022-08-11 22:40:41 +0900

    Create `**/db.providers.js`

---
+++ src/modules/providers/db.providers.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2/promise');
+const MysqlEnv = require('../../models/env/mysql.env');
+
+class DatabaseProvider {
+    /** @type { mysql.Pool } */
+    static pool;
+
+    constructor() {}
+
+    /** @type { MysqlEnv } mysqlEnv */
+    static initialize(mysqlEnv) {
+        if (this.pool) return this.pool;
+
+        this.pool = mysql.createPool({
+            host: mysqlEnv.HOST,
+            user: mysqlEnv.USER,
+            database: mysqlEnv.DATABASE,
+            password: mysqlEnv.PASSWORD,
+            waitForConnections: mysqlEnv.WAIT_FOR_CONNECTION,
+            connectionLimit: mysqlEnv.CONNECTION_LIMIT,
+        });
+
+        return this.pool;
+    }
+}
+
+module.exports = DatabaseProvider;
