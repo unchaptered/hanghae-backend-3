53b53d25c8ad7bd5fff26297bba998cdb9604797
Unchaptered <workstation19961002@gmail.com>
2022-08-11 22:41:27 +0900

    Update `src/index.js`, 변경 내역 반영

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const pool = require('./db');
-
-pool.getConnection()
-    .then(() => console.log('연결 성공'))
-    .catch((err) => console.log('연결 실패', err));
 
 const authRouter = require('./layers/routers/auth.router');
 const articleRouter = require('./layers/routers/article.router');
 const commentRouter = require('./layers/routers/comment.router');
 const sampleRouter = require('./layers/routers/sample.router');
+
+const { EnvironmentProvider, DatabaseProvider } = require('./modules/_.loader');
+
+const path = EnvironmentProvider.switchEvnPath();
+const env = EnvironmentProvider.setEnvInstance();
+const pool = DatabaseProvider.initialize(env.mysqlEnv);
+
+pool.getConnection()
+    .then(() => console.log('연결 성공'))
+    .catch((err) => console.log('연결 실패', err));
 
 const app = express();
 
