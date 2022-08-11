4ddf76485103f3b2b5a9fe424009013b8f2cf29a
Unchaptered <workstation19961002@gmail.com>
2022-08-11 11:58:32 +0900

    Modify `**/jwt.js`, 미사용 중인 jwt 미들웨어 require 구문 삭제

--- src/modules/jwt.js
+++ src/modules/jwt.js
@@ -1,15 +1,12 @@
 const jwt = require("jsonwebtoken");
 require('dotenv/config');
 
-const authRepository = require("../layers/repositories/auth.repository");
-const pool = require('../../src/db');
-
 module.exports = async (req, res, next) => {
 
     try {
         
         const { authorization } = req.headers;
-        const [authType, authToken] = (authorization || "").split(" ");
+        const [ authType, authToken ] = (authorization || "").split(" ");
 
         if (!authToken || authType !== "Bearer") {
             throw new Error("로그인 후 이용 가능한 기능입니다.");
